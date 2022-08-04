72b15de1ee49bee2c1c9e50146ddc7c3113f6a87
vivian <vivian.v18723@gmail.com>
2022-08-04 12:18:45 +0100

    make try catch blocks asynchronous

--- api/controllers/quotes.js
+++ api/controllers/quotes.js
@@ -2,31 +2,31 @@ const router = require("express").Router();
 
 const Quote = require("../models/Quote");
 
-router.get("/random", (req, res) => {
-    try {
-        const quote = Quote.showRandom();
-        res.json(quote);
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
+router.get("/random", async (req, res) => {
+  try {
+    const quote = await Quote.showRandom();
+    res.json(quote);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
-router.get("/", (req, res) => {
-    try {
-        const quotes = Quote.showAll();
-        res.json(quotes);
-    } catch (e) {
-        res.status(500).json({ error: e.message });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const quotes = await Quote.showAll();
+    res.json(quotes);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
-router.get("/:id", (req, res) => {
-    try {
-        const quote = Quote.show(req.params.id);
-        res.json(quote);
-    } catch (e) {
-        res.status(404).json({ error: e.message });
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const quote = await Quote.show(req.params.id);
+    res.json(quote);
+  } catch (e) {
+    res.status(404).json({ error: e.message });
+  }
 });
 
 module.exports = router;
