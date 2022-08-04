5d8399d28d1e71efc0dffbabdef084ad27511006
vivian <vivian.v18723@gmail.com>
2022-08-04 12:19:07 +0100

    make functions work & asynchronous

--- api/models/Quote.js
+++ api/models/Quote.js
@@ -1,31 +1,59 @@
-const quotesData = require("../data/quotes");
+const { Pool } = require("pg");
+const db = new Pool();
 
 class Quote {
-    constructor(data) {
-        [this.quote, this.author] = data.split("-");
-    }
+  constructor(data) {
+    this.quote = data.quote;
+    this.author = data.author;
 
-    static showAll() {
-        const quotes = quotesData.map((q) => new Quote(q));
-        if (!quotes.length) {
-            throw new Error("No quotes found");
-        }
-        return quotes;
-    }
+    // [this.quote, this.author] = data.split("-");
+  }
 
-    static showRandom() {
-        const randomId = Math.floor(Math.random() * quotesData.length);
-        return new Quote(quotesData[randomId]);
-    }
+  static async showAll() {
+    const { rows } = await db.query("SELECT * FROM quotes");
+    const quotes = rows.map((singleQuote) => new Quote(singleQuote));
+    console.log("quotes:", rows);
 
-    static show(id) {
-        // Check that user input is in the range of allowed indexes for the quotes array
-        if (id >= 1 && id <= quotesData.length) {
-            return new Quote(quotesData[id - 1]);
-        } else {
-            throw new Error("Quote not found");
-        }
+    if (!quotes.length) {
+      throw new Error("No quotes found");
     }
+    return quotes;
+  }
+
+  static async showRandom() {
+    /* (another way)
+        // get all elements in rows
+        const { rows } = await db.query("SELECT * FROM quotes");
+        // create random number
+        const randomId = Math.floor(Math.random() * rows.length); 
+        console.log("random quote:", rows[randomId]);
+        return new Quote(rows[randomId]);
+    */
+
+    const { rows } = await db.query("SELECT * from quotes ORDER BY RANDOM() LIMIT 1");
+    console.log("random quote:", rows);
+
+    if (rows[0]) {
+      return new Quote(rows[0]);
+    } else {
+      throw new Error("Oops, something went wrong on our side");
+    }
+  }
+
+  // need 'async' as 'await' is used below
+  static async show(id) {
+    // get all
+    const { rows } = await db.query("SELECT * FROM quotes WHERE id = $1", [id]);
+    console.log("quotes:", rows[0]);
+
+    // Check that user input is in the range of allowed indexes for the quotes array
+    // 'row[0]' returns a falsy value
+    if (rows[0]) {
+      return new Quote(rows[0]);
+    } else {
+      throw new Error("Quote not found");
+    }
+  }
 }
 
 module.exports = Quote;
