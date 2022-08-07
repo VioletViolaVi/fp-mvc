e165b5224b1b6e48743f96c4257fa269b908fcc3
vivian <vivian.v18723@gmail.com>
2022-08-07 18:15:46 +0100

    add log statement

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000;
 
 // To do: Get the server running
 app.listen(port, () => {
-  `Express listening on port ${port}...`;
+  console.log(`Express listening on port ${port}...`);
 });
